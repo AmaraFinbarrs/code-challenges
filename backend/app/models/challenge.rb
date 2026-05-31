aa0a0ffc4948778f0900e504581bc6544dd5074b
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2026-05-31 23:56:10 +0100

    Add validation to challenges model

--- backend/app/models/challenge.rb
+++ backend/app/models/challenge.rb
@@ -1,2 +1,6 @@
 class Challenge < ApplicationRecord
+  validate :title, presence: true
+  validate :description, presence: true
+  validate :start_date, presence: true
+  validate :end_date, presence: true
 end
