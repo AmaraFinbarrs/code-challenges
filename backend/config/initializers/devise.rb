099a098c1a7411729a6bbe57a72de2ce38047186
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2026-06-02 02:21:44 +0100

    Further jwt setup for seamless creation, dispatch and revocation.

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -312,4 +312,20 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # To create, dispatch and revoke jwt tokens.
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.dispatch_requests = [
+      [ "POST", %r{^/login$} ],
+      [ "POST", %r{^/signup$} ]
+    ]
+    jwt.revocation_requests = [
+      [ "DELETE", %r{^/logout$} ],
+      [ "DELETE", %r{^/signup$} ]
+    ]
+
+    # Manually set token expiration time
+    # jwt.expiration_time = 1.day.to_i
+  end
 end
