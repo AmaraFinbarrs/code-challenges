7bfa211dce70ff11373d0609e165e0acbf8c322e
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2026-06-02 02:49:35 +0100

    Help api-only app handle sessions

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  # Allows your API controllers to read/write session data
+  include ActionController::Cookies
+  
+  # For Rails 7+, this helper natively restores session access
+  include ActionController::Helpers
 end
